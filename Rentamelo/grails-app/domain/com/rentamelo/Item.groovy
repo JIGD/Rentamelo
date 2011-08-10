48f56504676577255172d24b8608637f9e1a7b9e
igonzalez <igonzalez@nearsoft.com>
2011-08-10 16:34:07 -0700

    skl;jsadf

--- Rentamelo/grails-app/domain/com/rentamelo/Item.groovy
+++ Rentamelo/grails-app/domain/com/rentamelo/Item.groovy
@@ -2,12 +2,13 @@ package com.rentamelo
 
 class Item {
 	
-	long id = 0L
+	
 	String name
-	Date deadLine //fecha en que acaba el anuncio (si es que acaba)
-	Date dateCreated //fecha en que fue creado
 	String summary // vista pequeña
 	String details // detalles
+	long id = 0L
+	Date deadLine //fecha en que acaba el anuncio (si es que acaba)
+	Date dateCreated //fecha en que fue creado
 	byte[] photo //foto del articulo
 	boolean isSent = false // para saber si se envia o no 
 	
--- Rentamelo/test/unit/com/rentamelo/ItemTests.groovy
+++ Rentamelo/test/unit/com/rentamelo/ItemTests.groovy
@@ -16,6 +16,14 @@ class ItemTests extends GrailsUnitTestCase {
     }
 	
 	void testDatabase(){
-		assertEquals 
+		}
+	
+	void testCRUD(){
+		def i = new Item(name:"algo", summary:"Este es un item que quiero rentar", 
+			details:"Aqui detallo muy bien que ondas con lo que quiero rentar sin pasarme de los mil caracteres", 
+			id:1231, photo:123, dateCreated: new Date(), deadLine: null, isSent:true )
+		i.save()
+		assert Item.get(b.id) != null
+		assert i.name == "algo"
 		}
 }
