ed3e9ecec33712d69a681133e57f09a6d5474cca
Oscar Ulloa <oscar.ulloa@gmail.com>
2011-08-10 18:11:17 -0700

    ahora si, se agrego el domain class user

---
+++ Rentamelo/grails-app/domain/com/rentamelo/User.groovy
@@ -0,0 +1,19 @@
+package com.rentamelo
+
+class User {
+	
+	String username
+	String password
+	String fullName
+	String address1
+	String address2
+	
+	static hasMany = [items:Item]
+	
+    static constraints = {
+    	username blank:false, unique:true
+		password blank:false, password:true
+		fullName blank:false
+		address1 blank:false		
+	}
+}
