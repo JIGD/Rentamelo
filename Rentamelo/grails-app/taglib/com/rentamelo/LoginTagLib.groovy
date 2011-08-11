d5b43578a94626e1d186584b6bd410d2889af45a
Oscar Ulloa <oscar.ulloa@gmail.com>
2011-08-11 11:12:47 -0700

    Taglib de Login

---
+++ Rentamelo/grails-app/taglib/com/rentamelo/LoginTagLib.groovy
@@ -0,0 +1,13 @@
+package com.rentamelo
+
+class LoginTagLib {
+	static namespace = "login"
+	def loginControl = {
+		if(session.user){
+		  out << "Hello ${session.user.fullName} "
+		  out << """[${link(action:"logout", controller:"user"){"Logout"}}]"""
+		} else {
+		  out << """[${link(action:"login", controller:"user"){"Login"}}]"""
+		}
+	  }
+}
---
+++ Rentamelo/test/unit/rentamelo/LoginTagLibTests.groovy
@@ -0,0 +1,17 @@
+package rentamelo
+
+import grails.test.*
+
+class LoginTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
