320e0c88d6c985aff981f388e64f9bfa612f1801
Oscar Ulloa <oscar.ulloa@gmail.com>
2011-08-15 18:40:36 -0700

    Cambios a seguridad del usuario. Se cambio el template de main que normalmente se aplica a todas las paginas, para que muestre un mensaje si el usuario está loggeado, junto con un link para hacer el logout.

--- Rentamelo/grails-app/conf/BootStrap.groovy
+++ Rentamelo/grails-app/conf/BootStrap.groovy
@@ -1,20 +1,24 @@
 import com.rentamelo.User
 import com.rentamelo.SecUser
+import com.rentamelo.SecRole
+import com.rentamelo.SecUserSecRole
+import com.rentamelo.*
 
 class BootStrap {
 	def springSecurityService
     def init = { servletContext ->
 		
-		def user = new User(username:"usuario01",password:"password",fullName:"Qwerty Asdf",address1:"z", email:"correo@email.com")
-		user.save(failOnError:true)
+		def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new SecRole(authority: 'ROLE_ADMIN').save(failOnError: true)
 		
-		def SecUser secUser = new SecUser(username: 'usuario01', 
-										  password: springSecurityService.encodePassword('password'),
-										  enabled:true).save(failOnError:true)
-		
-	    secUser = new SecUser(username: 'admin',
-								      password: springSecurityService.encodePassword('admin'),
-							          enabled:true).save(failOnError:true)
+		def adminUser = SecUser.findByUsername('admin') ?: new SecUser(
+							username: 'admin',
+							password: 'admin',
+							//password: springSecurityService.encodePassword('admin'),
+							enabled: true).save(failOnError: true)
+			 
+					if (!adminUser.authorities.contains(adminRole)) {
+						SecUserSecRole.create adminUser, adminRole
+					}
 			
 	}
     def destroy = {
