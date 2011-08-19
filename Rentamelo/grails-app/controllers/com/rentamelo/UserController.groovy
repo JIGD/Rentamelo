ab8b8684673b6803e02815fec286f0b85deb56b9
Oscar Ulloa <oscar.ulloa@gmail.com>
2011-08-19 16:42:54 -0700

    Limpiando el codigo y otros miscelaneos

--- Rentamelo/grails-app/controllers/com/rentamelo/UserController.groovy
+++ Rentamelo/grails-app/controllers/com/rentamelo/UserController.groovy
@@ -6,22 +6,4 @@ class UserController {
 	
 	def login = {}
 	
-	def authenticate = {
-		def user = User.findByUsernameAndPassword(params.username,params.password)
-		if(user){
-			session.user = user
-			flash.message = "Bienvenido a Rentamelo, ${user.fullName}!"	
-			redirect(controller:"user",action:"list")
-		}
-		else{
-			flash.message = "Lo siento, su nombre de usuario y/o contraseña están incorrectos"
-			redirect(action:"login")
-		}
-		
-	}
-	def logout = {
-		flash.message = "Adios, ${user.fullName}"
-		session.user = null
-		redirect(controller:"user",action:"list")
-	}
 }
