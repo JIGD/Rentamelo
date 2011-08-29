6b9a733f14db737fd1aaf27e51d128e15db41593
igonzalez <igonzalez@nearsoft.com>
2011-08-29 13:40:09 -0700

    Category y sus tests

---
+++ Rentamelo/test/integration/com/rentamelo/CategoryIntTests.groovy
@@ -0,0 +1,19 @@
+package com.rentamelo
+
+import grails.test.*
+
+class CategoryIntTests extends GroovyTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+	
+	protected void testSomething() {
+		def testInstances=[new Category(name:"CategoriaP").save(), new Category(name:"CategoriaH").save()]
+		mockDomain(Category, testInstances)
+		assertEquals(2, Category.count())
+	  }
+}
